d39e9357f96f4f7859c2a52f0206594353f7af76
ACatBeyondTheButton <134506097+ACatBeyondTheButton@users.noreply.github.com>
2026-09-20 19:09:21 +0100

    Add Commander.js and first command: init

--- bin/fairywhisper.js
+++ bin/fairywhisper.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log("✨ FairyWhisper CLI is alive ✨");
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('fairywhisper')
+  .description('A magical project bootstrapping CLI')
+  .version('1.0.0');
+
+program
+  .command('init')
+  .description('Initialize a new project ritual')
+  .action(() => {
+    console.log("🪄 Running the FairyWhisper init ritual...");
+  });
+
+  program.parse();
